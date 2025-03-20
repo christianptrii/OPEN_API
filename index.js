91237190bd9ece861737e943723ffc7ca9ed433f
Christian Putri <christianptrii@gmail.com>
2025-03-20 14:59:58 +0800

    Membuat HTTP server sederhana dgn node:http

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+});
+
+server.listen(4000, () => console.log('Server berjalan di http://localhost:4000'));
